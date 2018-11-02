66fc718b4dcfb9f78374900bad2d126f2501161c
Jani <janimarton22@gmail.com>
2018-11-02 11:20:50 +0100

    in Product.java: initiation, id increase implemented in the constructor AND in the Webshop.java tested out.

--- src/com/codecool/uml/overloading/Product.java
+++ src/com/codecool/uml/overloading/Product.java
@@ -1,4 +1,33 @@
 package com.codecool.uml.overloading;
 
+import java.util.Currency;
+import java.util.List;
+
 public class Product {
+
+    private static int idCounter = 0;
+    private int id;
+    private String name;
+    private float defaultPrice;
+    private Currency defaultCurrency;
+    private ProductCategory productCategory;
+    private Supplier supplier;
+    private List<Product> productList;
+
+    private int idIncreaseBy1(int id){
+        idCounter++;
+        id = idCounter;
+        return id;
+    }
+
+
+    public Product() {
+        this.id = idIncreaseBy1(id);
+
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
 }
--- src/com/codecool/uml/overloading/Webshop.java
+++ src/com/codecool/uml/overloading/Webshop.java
@@ -5,5 +5,14 @@ public class Webshop {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("It's working");
+        Product testProduct = new Product();
+        System.out.println("The testProducts id: "+testProduct.getId());
+        Product testProduct2 = new Product();
+        System.out.println("The testProducts id: "+testProduct2.getId());
+        Product testProduct3 = new Product();
+        System.out.println("The testProducts id: "+testProduct3.getId());
+        Product testProduct4 = new Product();
+        System.out.println("The testProducts id: "+testProduct4.getId());
+
     }
 }
