5fefd0ef45f67518e4e95e6827c077c11b7d423c
Jani <janimarton22@gmail.com>
2018-11-05 10:54:27 +0100

    in ProductCategory.java: private variables initialized

--- src/com/codecool/uml/overloading/ProductCategory.java
+++ src/com/codecool/uml/overloading/ProductCategory.java
@@ -1,4 +1,15 @@
 package com.codecool.uml.overloading;
 
 public class ProductCategory {
+
+
+    private int id;
+    private String name;
+    private String department;
+    private String description;
+
+
+
+
+
 }
