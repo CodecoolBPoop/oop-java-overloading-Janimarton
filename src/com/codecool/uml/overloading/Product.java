d7237493fffb19314dc712e17e54c4cae1a18a89
Jani <janimarton22@gmail.com>
2018-11-05 10:48:43 +0100

    in Product.java: all required getter and setter implemented

--- src/com/codecool/uml/overloading/Product.java
+++ src/com/codecool/uml/overloading/Product.java
@@ -26,8 +26,74 @@ public class Product {
 
     }
 
+    public Product(String name, float defaultPrice, Currency defaultCurrency) {
+        this.id = idIncreaseBy1(id);
+    }
+
+
     public int getId(){
         return this.id;
     }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getDefaultPrice(){
+        return this.defaultPrice;
+    }
+
+
+    public void setDefaultPrice(float defaultPrice){
+        this.defaultPrice = defaultPrice;
+    }
+
+    public Currency getDefaultCurrency(){
+        return this.defaultCurrency;
+    }
+
+    public void setDefaultCurrency(Currency defaultCurrency){
+        this.defaultCurrency = defaultCurrency;
+    }
+
+    public ProductCategory getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(ProductCategory productCategory){
+        this.productCategory = productCategory;
+    }
+
+    public Supplier getSupplier(){
+        return this.supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public List<Product> getAllProductsBy(ProductCategory productCategory){
+        return productList;
+    }
+
+    public List<Product> getAllProductsBy(Supplier supplier){
+        return productList;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", defaultPrice=" + defaultPrice +
+                ", defaultCurrency=" + defaultCurrency +
+                ", productCategory=" + productCategory +
+                ", supplier=" + supplier +
+                ", productList=" + productList +
+                '}';
+    }
 }
